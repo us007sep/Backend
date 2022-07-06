7b4da0456eac562f8e4c989d44caad43db421137
Uday <us007sep@gmail.com>
2022-07-06 15:01:03 +0530

    Initial Commit

---
+++ SimpleNode.ts
@@ -0,0 +1,11 @@
+import * as http from 'http';
+
+const server =  http.createServer((request ,response) => {
+    if(request.url === '/a')  
+    response.write("Welcome to Backend");
+    else    
+    response.write("Invalid URL");
+    response.end();
+});
+
+server.listen(3005 , () => console.log("Server 3005 running..."));
